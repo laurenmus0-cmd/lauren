dc77e792b87116be5a3417c450668cdbf8664d9c
laurenmus0-cmd <laurenmus0@gmail.com>
2026-02-03 16:43:17 +0300

    template

---
+++ pyhton/datypes.py
@@ -0,0 +1,39 @@
+#strings-a sequence of characters
+message="how are you"
+first_name="john"
+last_name="mwaniki"
+print(message)
+#type()
+print(type(message))
+print(type(first_name))
+#integers-whole numbers 45,7,90
+num=87
+print(num)
+print(type(num))
+#float-numbers with decimal places 30.8
+tax=16.6
+print(tax)
+print(type(tax))
+#boolean-True or False
+is_student=True
+print(is_student)
+print(type(is_student))
+#type conversion ,int(),str(),float()
+chrome="23"
+print(type(chrome))
+#int()
+mynum=int(chrome)
+print(mynum)
+print(type(mynum))
+#float()
+moxilla=67
+print(type(moxilla))
+#float()
+myfloat=float(moxilla)
+print(myfloat)
+print(type(myfloat))
+#str()
+x=56
+newx=str(x)
+print(newx)
+print(type(newx))
---
+++ pyhton/intro.py
@@ -0,0 +1,29 @@
+#display text
+print("hello world")
+print("my name is lauren")
+print(98)
+
+#variables- are named containers used to store value
+#variablename=value
+student_name = "lauren"
+age=18
+course="python"
+place="thika"
+school="emobilis"
+#display the value
+print(student_name)
+print(age)
+print(course)
+#dsplay strings and variables
+print("hello, my name is",student_name)
+print("i am",age,"years old")
+print("today ia m learning",course)
+print("i study at",school)
+print("i live at",place)
+print("my name is",student_name,"i am",age,"years old. i live at",place,)
+#reasigning a variable value
+age=20
+print(age)
+"""this is
+a multiline
+comment"""
